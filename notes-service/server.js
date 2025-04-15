a2363b48e5b05a16736c92e2ebe2bde357ba2ee2
MalikaIT21277122 <IT21277122@my.sliit.lk>
2025-04-15 23:04:33 +0530

    Added notes service model js file.

--- notes-service/server.js
+++ notes-service/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const Note = require('./models/Note');
 
 const app = express();
 app.use(express.json());
@@ -9,7 +10,21 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-app.get('/', (req, res) => res.send('Service is up and running!'));
+app.get('/', (req, res) => res.send('notes-service is running!'));
+
+// Create Note
+app.post('/notes', async (req, res) => {
+  const note = new Note(req.body);
+  await note.save();
+  res.send(note);
+});
+
+// Get all Notes
+app.get('/notes', async (req, res) => {
+  const notes = await Note.find();
+  res.send(notes);
+});
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`notes-service running on port ${PORT}`));
