f1d598df82ae0684989435459b7e777e45885465
bimsara weerakoon <99312076+it21303302b@users.noreply.github.com>
2025-04-20 18:23:48 +0530

    test

--- notes-service/server.js
+++ notes-service/server.js
@@ -12,7 +12,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get('/', (req, res) => res.send('notes-service is running!'));
 
-// Create Note
+// Create Note 
+
 app.post('/notes', async (req, res) => {
   const note = new Note(req.body);
   await note.save();
